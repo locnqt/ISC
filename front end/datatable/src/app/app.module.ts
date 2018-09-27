7ea6c379a26567f746af30ecca663d952d6c2ea5
locnqt <locnqt@gmail.com>
2018-09-27 15:25:14 +0700

    add button extension angular-datatables

--- front end/datatable/src/app/app.module.ts
+++ front end/datatable/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DataTablesModule } from 'angular-datatables';
 import { AppComponent } from './app.component';
-import { WithAjaxComponent} from "./with-ajax.component"
+import { ButtonExtensionComponent } from './button-extension/button-extension.component'
 // import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WithAjaxComponent
+    ButtonExtensionComponent
     // TableComponent
   ],
   imports: [
