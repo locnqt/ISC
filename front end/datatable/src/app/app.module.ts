fb9f7648283c25332d661245b6b5fb6b084f4d48
locnqt <locnqt@gmail.com>
2018-09-27 14:14:51 +0700

    edit

--- front end/datatable/src/app/app.module.ts
+++ front end/datatable/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DataTablesModule } from 'angular-datatables';
 import { AppComponent } from './app.component';
+import { WithAjaxComponent} from "./with-ajax.component"
+// import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WithAjaxComponent
+    // TableComponent
   ],
   imports: [
     BrowserModule,
