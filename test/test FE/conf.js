834ff1bf88f6866cd6ce3f009f956f9a5dadae5f
Nguyen Quang Tuan Loc <locnqt@gmail.com>
2018-10-16 16:57:35 +0700

    test

--- test/test FE/conf.js
+++ test/test FE/conf.js
@@ -1,5 +1,13 @@
   exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js']
+    specs: ['spec.js'],
+    // capabilities: {
+    //   browserName: 'firefox'
+    // },
+    // multiCapabilities: [{
+    //   browserName: 'firefox'
+    // }, {
+    //   browserName: 'chrome'
+    // }]
   }
--- test/test FE/spec.js
+++ test/test FE/spec.js
@@ -1,7 +1,50 @@
+
 describe('Protractor Demo App', function() {
-    it('should have a title', function() {
-      browser.get('http://juliemr.github.io/protractor-demo/');
-  
-      expect(browser.getTitle()).toEqual('Super Calculator');
-    });
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
+
+  beforeEach(function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
   });
+
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+
+  it('should add one and two', function() {
+    add(1,2);
+  });
+
+  it('should multi five and six', function() {
+    firstNumber.sendKeys(5);
+    secondNumber.sendKeys(6);
+
+    var select = element(by.model('operator'));
+    select.$('[value="MULTIPLICATION"]').click();
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('30');
+  });
+
+  it('should read the value from an input', function() {
+    firstNumber.sendKeys(1);
+    expect(firstNumber.getAttribute('value')).toEqual('1');
+  });
+
+  it('should have a history', function() {
+    add(1, 2);
+    add(3, 4);
+
+    expect(history.last().getText()).toContain('1 + 2');
+    expect(history.first().getText()).toContain('3 + 4'); // This is wrong!
+  });
+});
